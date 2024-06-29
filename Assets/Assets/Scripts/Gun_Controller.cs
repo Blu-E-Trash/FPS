a47ccfdff18556abfe320be420d1b8677b89d85f
Blu_E_rash <163538035+Blu-E-Trash@users.noreply.github.com>
2024-06-30 08:21:04 +0900

    Made Gun
    Made Gun

---
+++ Assets/Assets/Scripts/Gun_Controller.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();    
+    }
+    void Update()
+    {
+        GunFireRateCalc();
+        TryFire();
+
+    }
+    private void GunFireRateCalc()
+    {
+        if(currentFireRate > 0)
+        {
+            currentFireRate-= Time.deltaTime;
+        }
+    }
+    private void TryFire()
+    {
+        if (Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+    private void Shoot()
+    {
+        PlaySE(currentGun.fire_Sound);
+        currentGun.muzzleFlash.Play();
+        Debug.Log("ÃÑ½ô");
+
+    }
+    private void PlaySE(AudioClip _clip)
+    {
+        audioSource.clip = _clip;
+        audioSource.Play();
+    }
+}
