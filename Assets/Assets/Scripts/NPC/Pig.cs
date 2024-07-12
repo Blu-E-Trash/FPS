399ca655a5e5e57b996e4fef5a42a9c290dda16f
Blu_E_rash <163538035+Blu-E-Trash@users.noreply.github.com>
2024-07-13 03:00:31 +0900

    도야지
    도야지

---
+++ Assets/Assets/Scripts/NPC/Pig.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    [SerializeField]
+    private string animalName;
+    [SerializeField]
+    private int hp;
+    [SerializeField]
+    private float walkSpeed;
+
+    private Vector3 direction;
+
+    private bool isAction;
+    private bool isWalking;
+
+    [SerializeField]
+    private float walkTime;
+    [SerializeField]
+    private float waitTime;
+    private float currentTime;
+
+    //필컴
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private Rigidbody rigid;
+    [SerializeField]
+    private BoxCollider boxCol;
+
+    void Start()
+    {
+        currentTime = waitTime;
+        isAction = true;
+    }
+    void Update()
+    {
+        Move();
+        Rotation();
+        ElapseTime();    
+    }
+    private void Move()
+    {
+        if (isWalking)
+        {
+            rigid.MovePosition(transform.position + (transform.forward * walkSpeed * Time.deltaTime));
+        }
+    }
+    private void Rotation()
+    {
+        if (isWalking)
+        {
+            Vector3 _rotation = Vector3.Lerp(transform.eulerAngles, direction, 0.01f);
+            rigid.MoveRotation(Quaternion.Euler(_rotation));
+        }
+    }
+    private void ElapseTime()
+    {
+        if (isAction)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                ReSet();
+            }
+        }
+    }
+    private void ReSet()
+    {
+        isWalking = false;
+        isAction = true;
+        anim.SetBool("Walking", isWalking);
+        direction.Set(0f, Random.Range(0f, 360f), 0f);
+        RandomAction();
+    }
+    private void RandomAction()
+    {
+        isAction = true;
+
+        int _random = Random.Range(0, 4); // 대기, 풀뜯기, 두리번, 걷기
+        if (_random == 0)
+            Wait();
+        else if (_random == 1)
+            Eat();
+        else if (_random == 2)
+            Peek();
+        else if (_random == 3)
+            TryWalk();
+    }
+    private void Wait()
+    {
+        currentTime = waitTime;
+        Debug.Log("대기");
+    }
+    private void Eat()
+    {
+        currentTime = waitTime;
+        anim.SetTrigger("Eat");
+        Debug.Log("풀뜯기");
+    }
+    private void Peek()
+    {
+        currentTime = waitTime;
+        anim.SetTrigger("Peek");
+        Debug.Log("두리번");
+    }
+    private void TryWalk()
+    {
+        isWalking = true;
+        currentTime = walkTime;
+        anim.SetBool("Walking", isWalking);
+        Debug.Log("걷기");
+    }
+}
